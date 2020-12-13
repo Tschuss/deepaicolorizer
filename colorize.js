4daef6f987adad0e56dc3d7ba48987674648a1f9
Tschuss <jiglesgom@gmail.com>
2020-12-13 19:31:30 +0100

    fix: recorre recursivo los directorios

--- colorize.js
+++ colorize.js
@@ -12,87 +12,78 @@ deepai.setApiKey('bc2df53a-ee00-4283-a500-d71c9c11b1db');
 
 const photo_dir = process.argv[2];
 
-console.log ("directorio a procesar: "+ photo_dir);
+processDir(photo_dir);
 
-var filelist =[];
+function processDir(dir) {
+    return new Promise((resolve) => {
+        console.log ("Empezamos a procesar el directorio ["+ dir+"]", 'color: #00ff00');
 
-fs.readdir(photo_dir, (err, files) => {
-    files.forEach(file => {
-      console.log("Procesa la photo:" +file);
-      (async function() { 
-        var resp;
-        var photo=photo_dir+"\\"+file;
-        resp = await deepai.callStandardApi("colorizer", {
-            image: fs.createReadStream(photo),
-        }).catch(error => {
-            console.error('ERROR ',error);
-        });
-        return new Promise((resolve) => {
-            var url=resp.output_url;
-            var photo1=photo.substr(0,photo.lastIndexOf('.'));
-            var localPath=photo1+'_color.jpg';
-    
-            request.head(url, function(err2, res, body){
-                console.log ("escribe en disco: "+ localPath);
-                request(url).pipe(fs.createWriteStream(localPath));
+        var count = {numdir:0, numfiles:0};
+        fs.readdir(dir, (err, files) => {
+            files.forEach(file => {
+                var path=dir+"\\"+file;
+                console.log ("Contenido: ["+ path+"]");
+
+                fs.stat(path, (error, stats) => {
+                    if (error) {
+                        console.error (error);
+                    } else {
+                        if (stats && stats.isDirectory()) {
+                            count.numdir++;
+                            console.log (path+" es un directorio");
+                            processDir(path);
+                        } else {
+                            count.numfiles++
+                            processFile(path);
+                        }
+                    }
                 });
-            }).catch(error => {
-                console.error('ERROR ',error);
             });
-        })()
-    });
-  });
-  
-// var photo="C:\\Users\\jiglesias\\Pictures\\jesus.png";
-
-// (async function() {
-//     var resp;
-//     resp = await deepai.callStandardApi("colorizer", {
-//         image: fs.createReadStream(photo),
-//     }).catch(error => {
-//         console.error('ERROR ',error);
-//     });
-//     console.log(resp);
-//     //añadido para evitar un error al validar el certificado (que no salida en octubre y ahora aparece en noviembre 2020)
-//     //process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
-//     return new Promise((resolve) => {
-//         var url; //= "https://api.deepai.org/job-view-file/7c87cc27-cf07-4459-ad87-7f8e43478897/outputs/output.jpg"
-//         if (resp) {
-//             url=resp.output_url;
-//         }
-//         var photo1=photo.substr(0,photo.lastIndexOf('.'));
-//         var localPath=photo1+'_color.jpg';
-
-//         request.head(url, function(err, res, body){
-//             console.log('content-type:', res.headers['content-type']);
-//             console.log('content-length:', res.headers['content-length']);
-        
-//             request(url).pipe(fs.createWriteStream(localPath));
-//           });
-
-          
-
-//     }).catch(error => {
-//         console.error('ERROR ',error);
-//     });
-//})()
-
-/**
- 
- request(url, function (error, response, body) {
-
-
-            console.log (photo1+'_color.jpg');
-            //console.debug (body);
-            var buf = Buffer.from(body, 'binary');
-            fs.writeFileSync(photo1+'_color.jpg', buf);
-            //console.log (body);
- // ****** other
- 
-
-        var file = fs.createWriteStream(localPath);
-        console.log(url +' >> '+localPath)
-        var request = https.get(url, function(response) {
-            response.pipe(file);
         });
- */
+    });
+}
+
+async function processFile (file) {
+    try {
+        console.log ("Empezamos a procesar el archivo ["+ file +"]", 'color: #0000ff');
+
+        if (file.lastIndexOf("_color.jpg")>0) {
+            console.log ("Photo ya coloreada: "+file)
+        } else {
+            var resp;
+            var photo=file;
+            //(async function() { 
+                resp = await deepai.callStandardApi("colorizer", {
+                    image: fs.createReadStream(photo),
+                })
+                .catch(error => {
+                    console.error("Error en API:", 'color: #ff0000');
+                    console.error(error.request);
+                });
+                //return new Promise((resolve) => {
+                    if (resp) {
+                        var url=resp.output_url;
+                        var photo1=photo.substr(0,photo.lastIndexOf('.'));
+                        var localPath=photo1+'_color.jpg';
+                        try {
+                            request.head(url, function(err2, res, body){
+                                console.log ("escribe en disco: "+ localPath);
+                                request(url).pipe(fs.createWriteStream(localPath));
+                            });
+                        } catch(error) {
+                            console.error("Error en request: "+localPath, 'color: #ff0000');
+                            console.error(error.response);
+                        };
+                    } else {
+                        console.error ("API response no existe", 'color: #ff0000');
+                    }
+                //}).catch ( error => {
+                //    console.log(error);
+                //});
+            //});
+        }
+    }catch (error) {
+        console.error ("Error al procesar el fichero: "+file, 'color: #ff0000');
+        console.error (error);
+    }
+}
